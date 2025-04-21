60615477770924ac04ed7715804746e34baca355
ragashanmugamrg <ragashanmugam1997@gmail.com>
2025-04-21 19:26:45 +0530

    Add CI workflow to build and release APK

--- app/src/main/java/com/amp/nvamp/adapter/Songslistadapter.kt
+++ app/src/main/java/com/amp/nvamp/adapter/Songslistadapter.kt
@@ -1,20 +1,25 @@
 package com.amp.nvamp.adapter
 
 import android.view.LayoutInflater
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.OptIn
+import androidx.appcompat.view.ContextThemeWrapper
+import androidx.appcompat.widget.PopupMenu
 import androidx.media3.common.MediaItem
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.session.MediaController
 import androidx.recyclerview.widget.RecyclerView
 import com.amp.nvamp.MainActivity.Companion.playerViewModel
+import com.amp.nvamp.NvampApplication.Companion.context
 import com.amp.nvamp.R
 import com.amp.nvamp.utils.NvampUtils
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.google.android.material.button.MaterialButton
 import com.google.common.util.concurrent.ListenableFuture
 
 class Songslistadapter(
@@ -28,6 +33,8 @@ class Songslistadapter(
         val songartist: TextView = view.findViewById(R.id.artist)
         val songDuration: TextView = view.findViewById(R.id.duration)
         val songalbumart: ImageView = view.findViewById(R.id.imageView)
+
+        val options: MaterialButton = view.findViewById(R.id.options)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -54,6 +61,15 @@ class Songslistadapter(
             .placeholder(R.drawable.ic_songs_foreground)
             .into(holder.songalbumart)
 
+        holder.options.setOnClickListener { v ->
+            showPopupMenu(v,position)
+        }
+
+
+
+
+
+
         holder.itemView.setOnClickListener{
             if (medcontroller.isDone){
                 var controller = medcontroller.get()
@@ -64,5 +80,19 @@ class Songslistadapter(
 
         }
 
+    }
+
+    fun showPopupMenu(view: View,position: Int) {
+        val themedContext = ContextThemeWrapper(context, R.style.Base_Theme_Nvamp)
+        val popupMenu = PopupMenu(themedContext,view)
+        val inflater: MenuInflater = popupMenu.menuInflater
+        inflater.inflate(R.menu.song_menu,popupMenu.menu)
+        popupMenu.setOnMenuItemClickListener { item -> handleMenuClick() }
+        popupMenu.show()
+    }
+
+
+    fun handleMenuClick(): Boolean{
+        return false
     }
 }
--- app/src/main/java/com/amp/nvamp/playback/PlaybackService.kt
+++ app/src/main/java/com/amp/nvamp/playback/PlaybackService.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import android.media.AudioFocusRequest
 import android.media.AudioManager
+import android.media.audiofx.Equalizer
+import android.media.audiofx.LoudnessEnhancer
 import android.media.session.PlaybackState
 import android.util.Log
 import androidx.annotation.OptIn
@@ -13,6 +15,8 @@ import androidx.media3.common.C.WakeMode
 import androidx.media3.common.Player
 import androidx.media3.common.Tracks
 import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.DefaultLoadControl
+import androidx.media3.exoplayer.DefaultRenderersFactory
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
@@ -33,11 +37,38 @@ class PlaybackService : MediaSessionService(),Player.Listener,AudioManager.OnAud
     @OptIn(UnstableApi::class)
     override fun onCreate() {
         super.onCreate()
-        player = ExoPlayer.Builder(this).build()
+
+        val loadControl = DefaultLoadControl.Builder()
+            .setBufferDurationsMs(25000, 50000, 1000, 2000)
+            .build()
+
+
+        val audioAttributes = AudioAttributes.Builder()
+            .setUsage(C.USAGE_MEDIA)
+            .setContentType(C.AUDIO_CONTENT_TYPE_MUSIC)
+            .build()
+
+        player = ExoPlayer.Builder(this)
+            .setLoadControl(loadControl)
+            .setAudioAttributes(audioAttributes,true)
+            .build()
+
         player.addListener(this)
         player.setWakeMode(C.WAKE_MODE_LOCAL)
         sessionId = player.audioSessionId
         mediaSession = MediaSession.Builder(this, player).build()
+
+
+        val sessionId = player.audioSessionId
+        val loudnessEnhancer = LoudnessEnhancer(sessionId)
+        loudnessEnhancer.setTargetGain(1000) // Gain in millibels
+        loudnessEnhancer.enabled = true
+
+        val equalizer = Equalizer(0, player.audioSessionId)
+        equalizer.enabled = true
+
+
+
     }
 
 
@@ -52,9 +83,6 @@ class PlaybackService : MediaSessionService(),Player.Listener,AudioManager.OnAud
 
     override fun onTracksChanged(tracks: Tracks) {
         super.onTracksChanged(tracks)
-        if (!successfullyRetrievedAudioFocus()){
-
-        }
         Log.d("Service","Track Listerner")
 
     }
@@ -62,9 +90,6 @@ class PlaybackService : MediaSessionService(),Player.Listener,AudioManager.OnAud
 
     override fun onPlaybackStateChanged(playbackState: Int) {
         super.onPlaybackStateChanged(playbackState)
-        if (!successfullyRetrievedAudioFocus()){
-
-        }
 
 
     }
@@ -80,7 +105,7 @@ class PlaybackService : MediaSessionService(),Player.Listener,AudioManager.OnAud
             .setUsage(android.media.AudioAttributes.USAGE_MEDIA)
             .setContentType(android.media.AudioAttributes.CONTENT_TYPE_MUSIC)
             .build()
-        var audioFocusRequest: AudioFocusRequest = AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
+        var audioFocusRequest = AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
             .setAudioAttributes(audioAttributes)
             .setOnAudioFocusChangeListener(this)
             .build()
@@ -97,7 +122,10 @@ class PlaybackService : MediaSessionService(),Player.Listener,AudioManager.OnAud
     override fun onAudioFocusChange(position: Int) {
         when(position){
             AudioManager.AUDIOFOCUS_LOSS -> player.pause()
-            AudioManager.AUDIOFOCUS_GAIN -> player.play()
+            AudioManager.AUDIOFOCUS_GAIN -> {
+                player.volume = 1.0f
+                player.play()
+            }
             AudioManager.AUDIOFOCUS_LOSS_TRANSIENT -> player.pause()
             AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK -> player.volume = 1.0f
         }
